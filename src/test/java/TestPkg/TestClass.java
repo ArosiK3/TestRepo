6d5335c3f15cc979f48b19c134c6b8091d62c2ae
Arosi <aroshi.khatua96@gmail.com>
2025-07-19 11:32:31 +0530

    New code in new class file

--- src/test/java/TestPkg/TestClass.java
+++ src/test/java/TestPkg/TestClass.java
@@ -2,7 +2,11 @@ package TestPkg;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Adding two numbers");
+		int a = 10;
+		int b = a + 34;
+		int c = a + b;
+		System.out.println("Addition is: " + c);
 	}
 
 }
